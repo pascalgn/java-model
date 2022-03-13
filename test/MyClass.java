474718657daf2ba93b5426e152eb0a6dd889ab86
Pascal <pascal.gruen@gmail.com>
2022-03-13 12:28:32 +0100

    Update tests

--- test/MyClass.java
+++ test/MyClass.java
@@ -6,18 +6,25 @@ import test.annotate.*;
 @ClassAnnotation1("hello")
 @ClassAnnotation2(value = 1, text = "" + "", obj = @Complex())
 abstract class MyClass {
+    @interface Annotation1 {
+		String value() default "";
+	}
+
     @Annotation1
     abstract Map<@NotNull String, String> method1(int x);
 
-    abstract void method2(String a, @A @B(1.0) List<@Null String> b);
+    abstract void method2(final String a, @A @B(1.0) List<@Null String> b);
 
-    class InnerClass1<T1> {
-        class InnerClass2<T2> {
+    protected class InnerClass1<T1> {
+        class InnerClass2<T2 extends T1 & Serializable> {
+            private T2 field2;
         }
+
+        private T1 field1;
     }
 
     abstract InnerClass1<String>.InnerClass2<Number> method3();
 
-    private List<Map<Set<Integer>, List<Double>>> field1;
+    private final List<Map<Set<Integer>, List<Double>>> field1;
     private Set<? extends List<?>> field2;
 }
--- test/parse.test.ts
+++ test/parse.test.ts
@@ -1,20 +1,40 @@
-import { readdirSync, readFileSync, writeFileSync } from "fs";
+import process from "process";
+import { readFileSync, writeFileSync } from "fs";
 import { parse } from "../src/parse";
+import { Class, ObjectType, ResolvedType } from "../src/Project";
 
-describe("parse", () => {
+describe("Parser", () => {
   test("should parse the files", () => {
-    const files = readdirSync("test");
-    for (const file of files) {
-      if (file.endsWith(".java")) {
-        const source = readFileSync(`test/${file}`, "utf8");
-        const result = JSON.stringify(parse(source), undefined, 2) + "\n";
-        const output_file = `test/${file.slice(0, -5)}.json`;
-        const expected = readFileSync(output_file, "utf8");
-        if (result !== expected) {
-          writeFileSync(output_file, result, "utf8");
-          throw new Error("Result doesn't match expected output!");
+    const source = readFileSync("test/MyClass.java", "utf8");
+    const project = parse([source]);
+    const compilationUnit = project.findCompilationUnits("test")[0];
+
+    compareResult("test/MyClass.project.json", compilationUnit);
+
+    const types: Record<string, ResolvedType> = {};
+    compilationUnit.visitTypes((type) => {
+      if (type instanceof Class) {
+        for (const method of type.methods) {
+          if (method.type instanceof ObjectType) {
+            types[method.type.name] = method.type.resolve();
+          }
         }
       }
-    }
+    });
+
+    compareResult("test/MyClass.types.json", types);
   });
 });
+
+function compareResult(filename: string, result: unknown) {
+  const str = JSON.stringify(result, undefined, 2) + "\n";
+  const expected = readFileSync(filename, "utf8");
+  if (str !== expected) {
+    if (process.env.WRITE_RESULTS == undefined) {
+      throw new Error("Result doesn't match expected output!");
+    } else {
+      writeFileSync(filename, str, "utf8");
+      console.log("Result has been written to:", filename);
+    }
+  }
+}
